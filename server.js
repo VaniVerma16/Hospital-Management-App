04e6d51e835dcb8b3e23c7e0f181f7575ce81af0
vaniv <athv82237@gmail.com>
2024-05-14 00:15:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/hospital", {
+mongoose.connect("mongodb://localhost:27017/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
