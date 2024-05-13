9d1fb26cd0e7edbbb7e1e060889eec7dd2049349
vaniv <athv82237@gmail.com>
2024-05-14 01:04:28 +0530

    Revert "updated server.js to mongodb://localhost:27017"
    This reverts commit 62443748837eb629eb32888a4b31c5f7681f13fb.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose.connect("mongodb://localhost:27017/hospital", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
